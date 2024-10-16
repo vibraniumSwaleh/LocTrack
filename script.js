d65645af697107702e571c5d0e1130dfa2e6ddfb
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-10-16 23:40:25 +0100

    add CDN leaflet library

--- script.js
+++ script.js
@@ -13,14 +13,23 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
-    function (position)
-    {
-      const { latitude, longitude} = position.coords;
+    function (position) {
+      const { latitude, longitude } = position.coords;
       console.log(`https://www.google.ie/maps/@${latitude},${longitude}`);
     },
-    function ()
-    {
-      alert("Could not get your position!")
+    function () {
+      alert('Could not get your position!');
     }
   );
 }
+
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
