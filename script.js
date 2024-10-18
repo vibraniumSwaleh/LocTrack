fd8837f7da45c0873b6a85fbd84cb5edb14da68e
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-10-18 23:41:22 +0100

    add custom popup on marker for map

--- script.js
+++ script.js
@@ -15,21 +15,44 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
-      console.log(`https://www.google.ie/maps/@${latitude},${longitude}`);
+      //console.log(`https://www.google.ie/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      //console.log(map);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng,
+          clickCoords = [lat, lng];
+
+        L.marker(clickCoords)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Running')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position!');
     }
   );
 }
-
-
-const map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
