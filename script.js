9ba2e44dac20225133b5b3e1ab084eb670a68d4a
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-10-20 08:59:34 +0100

    complete setup for  workout objects based on type

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ class Cycling extends Workout {
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -102,20 +103,37 @@ class App {
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng,
+      workoutCoords = [lat, lng];
+    let workout;
+
+    const validInput = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
 
     if (type === 'running') {
       const cadence = +inputCadence.value;
       if (
-        !Number.isFinite(distance) ||
-        !Number.isFinite(duration) ||
-        !Number.isFinite(cadence)
+        !validInput(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
       )
         return alert('Input have to be positive numbers!');
+
+      workout = new Running(workoutCoords, distance, duration, cadence);
     }
 
     if (type === 'cycling') {
       const elevation = +inputElevation.value;
+      if (
+        !validInput(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Input have to be positive numbers!');
+
+      workout = new Cycling(workoutCoords, distance, duration, elevation);
     }
+
+    this.#workouts.push(workout);
+    console.log(workout);
 
     inputDistance.value =
       inputDuration.value =
@@ -123,10 +141,7 @@ class App {
       inputElevation.value =
         '';
 
-    const { lat, lng } = this.#mapEvent.latlng,
-      clickCoords = [lat, lng];
-
-    L.marker(clickCoords)
+    L.marker(workoutCoords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
